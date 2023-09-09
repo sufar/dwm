22d61fd63533c35b7c0bfc02e20aa44bec7c5ca4
sufar <fozoto@163.com>
2023-09-09 10:25:50 +0800

    add systray-6.4, status2d-systray-6.4, status2d-6.3 patches

--- dwm.c
+++ dwm.c
@@ -909,7 +909,8 @@ drawstatusbar(Monitor *m, int bh, char* stext) {
 	text = p;
 
 	w += 2; /* 1px padding on both sides */
-	ret = x = m->ww - w;
+  ret = m->ww - w;
+	x = m->ww - w - getsystraywidth();
 
 	drw_setscheme(drw, scheme[LENGTH(colors)]);
 	drw->scheme[ColFg] = scheme[SchemeNorm][ColFg];
